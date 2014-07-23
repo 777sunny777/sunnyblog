781689a07ece400d52b7bf8550bb4e129e84b2d7
sunny <sunnysunyan7@163.com>
2014-07-23 17:44:08 +0800

    fixed

--- js/blog.js
+++ js/blog.js
@@ -2,9 +2,12 @@ $(function(){
 
     $(".pul").slideUp("fast");
     $(".pul:eq(0)").slideDown("fast");
+    $(".ulp:eq(0)").children().removeClass('glyphicon-plus').addClass('glyphicon-minus');
 
     $(".ulp").click(function(){
         $(".pul").slideUp("normal");
+        $(".ulp").children().removeClass('glyphicon-minus').addClass('glyphicon-plus');
         $(this).next().slideDown("normal");
+        $(this).children().removeClass('glyphicon-plus').addClass('glyphicon-minus');
     });
 });
